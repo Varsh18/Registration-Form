adc37d0e7f0eb877362b6a57ec375905cc1611b6
varsh18 <varshiniravi1824@gmail.com>
2018-10-03 18:50:54 +0530

    Add header

--- designs.js
+++ designs.js
@@ -1,2 +1,10 @@
 /* set background color*/
 document.body.style.backgroundColor="rgba(24, 220, 255,1.0)";
+
+/*create heading for the webpage*/
+var h1=document.createElement("H1");
+var t=document.createTextNode("Registration Form");
+h1.style.textAlign="center";
+h1.style.color="#c56cf0";
+h1.appendChild(t);
+document.body.appendChild(h1);
